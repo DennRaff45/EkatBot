ee72f5142a571bfe2b9856fffe1566e477563cf3
Denis Salimov <denis_45@bk.ru>
2022-11-27 23:04:58 +0500

    added filter for bad words

--- EkatBot.py
+++ EkatBot.py
@@ -1,6 +1,9 @@
 import logging
 from aiogram import Bot, Dispatcher, executor, types
 import Parsing_site
+import json
+import string
+
 
 API_TOKEN = '5891307674:AAGPQ7xonAQjiafm_ADUGNIjKSJs-a55Pxg'
 
@@ -14,9 +17,21 @@ dp = Dispatcher(bot)
 async def send_welcome(message: types.Message):
     await message.answer("Hi! I'm EkatBot\nDon't know where to go tonight?.")
 
+"""Create a parser, which returns a list with events"""
 @dp.message_handler(commands=['show'])
 async def send_result(message: types.Message):
     await message.answer(f'Here the results:\n {Parsing_site.string_out}')
+
+
+"""Create a checker for bad words"""
+@dp.message_handler()
+async def bad_words_cheker(message: types.Message):
+    if {i.lower().translate(str.maketrans('','', string.punctuation)) for i in message.text.split(' ')}\
+            .intersection(set(json.load(open('cenz.json')))) != set():
+        await message.reply('Маты запрещены')
+        await message.delete()
+
+
 
 
 
